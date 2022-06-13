7a6517dd4852ab964355a46eecad881a38575805
yws <yws528819@163.com>
2022-06-13 14:52:18 +0800

    webSocket Test

---
+++ netty-pro/src/main/java/com/yws/netty/websocket/MyServer.java
@@ -0,0 +1,65 @@
+package com.yws.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class MyServer {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        try {
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //因为基于http协议，使用http的编码和解码器
+                            pipeline.addLast(new HttpServerCodec());
+                            //是以块方式写，添加ChunkedWriteHandler处理器
+                            pipeline.addLast(new ChunkedWriteHandler());
+                            /**
+                             * 说明：
+                             * 1.Http数据在传输过程中是分段，HttpObjectAggregator，可以将多个段聚合
+                             * 2.这就是为什么，当浏览器发送大量数据时，就会发送多次http请求
+                             */
+                            pipeline.addLast(new HttpObjectAggregator(8192));
+                            /**
+                             * 说明：
+                             * 1.对应websocket，他的数据是以帧(frame)形式传递
+                             * 2.可以看到WebSocketFrame下面有六个子类
+                             * 3.浏览器请求时 ws://localhost:8888/hello 表示请求的url
+                             * 4.WebSocketServerProtocolHandler 核心功能是将http协议升级为ws协议，保持长连接
+                             * 5.是通过一个状态码 101
+                             */
+                            pipeline.addLast(new WebSocketServerProtocolHandler("/hello"));
+                            //自定义的handler，处理业务逻辑
+                            pipeline.addLast(new MyTextWebSocketFrameHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ netty-pro/src/main/java/com/yws/netty/websocket/MyTextWebSocketFrameHandler.java
@@ -0,0 +1,36 @@
+package com.yws.netty.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+//TextWebSocketFrame 表示一个文本帧
+public class MyTextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("服务器收到消息：" + msg.text());
+        //回复消息
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间" + LocalDateTime.now() + " " + msg.text()));
+    }
+
+    //当web客户端连接后，触发方法
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        //id表示唯一值，LongText是唯一的，shortText不是唯一，可能重复
+        System.out.println("handlerAdded 被调用" + ctx.channel().id().asLongText());
+        System.out.println("handlerAdded 被调用" + ctx.channel().id().asShortText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved 被调用" + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生" + cause.getMessage());
+        ctx.close();//关闭连接
+    }
+}
